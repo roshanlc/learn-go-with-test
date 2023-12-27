bf2f6fda3aea0ce2ae18fb156ec350242ff4feb4
Roshan Lamichhane <roshan.lamichhane@grepsr.com>
2023-12-27 21:09:33 +0545

    hello world test

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
 package main
+
+const englishHelloPrefix = "Hello, "
+
+func main() {
+
+}
+
+func sayHello(name string) string {
+	return englishHelloPrefix + name
+}
--- main_test.go
+++ main_test.go
@@ -1,1 +1,12 @@
 package main
+
+import "testing"
+
+func TestSayHello(t *testing.T) {
+	name := "Ramu"
+	got := sayHello(name)
+	want := "Hello, " + name
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
