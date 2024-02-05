8333ae73045b425f2c0758a4bd366c038e0846f5
Roshan Lamichhane <roshan.lamichhane@grepsr.com>
2024-02-05 22:35:06 +0545

    update the hello world test

--- main_test.go
+++ main_test.go
@@ -3,10 +3,26 @@ package main
 import "testing"
 
 func TestSayHello(t *testing.T) {
-	name := "Ramu"
-	got := sayHello(name)
-	want := "Hello, " + name
+	t.Run("Saying hello to people", func(t *testing.T) {
+		name := "Ramu"
+		got := sayHello(name)
+		want := "Hello, " + name
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Saying 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := sayHello("")
+		want := "Hello, "
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+// checks for assertion that got and want are equal
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q,want %q", got, want)
 	}
 }
